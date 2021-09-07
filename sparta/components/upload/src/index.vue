8bd3ca30e9a4355e2685cf93de62201e39afe6e4
nijunjie <nijunjie@corp.netease.com>
2021-09-07 11:10:26 +0800

    feat: 🎸 Upload 默认参数适配跨境

--- sparta/components/upload/src/index.vue
+++ sparta/components/upload/src/index.vue
@@ -67,18 +67,18 @@
             <i v-else-if="item.status === 'fail'" class="sp-icon-warning-circle" />
             <i v-else class="sp-icon-file" />
             <a
-              v-if="item.url"
+              v-if="item.fileUrl"
               class="sp-upload-file__item-name"
-              :href="item.url"
-              :title="item.name"
+              :href="item.fileUrl"
+              :title="item.fileName"
               target="_blank"
-            >{{ item.name }}</a>
+            >{{ item.fileName }}</a>
             <!-- 文件名 -->
             <span
               v-else
-              :title="item.name"
+              :title="item.fileName"
               class="sp-upload-file__item-name"
-            >{{ item.name }}</span>
+            >{{ item.fileName }}</span>
             <!-- 错误提示 -->
             <span
               v-if="item.status === 'fail'"
@@ -110,18 +110,18 @@
           >
             <div class="sp-upload-picture__item-info">
               <img
-                v-if="item.status === 'success' && (item.type === 'image' || isIE9)"
+                v-if="item.status === 'success' && (item.fileType === 'image' || isIE9)"
                 :src="_getImgUrl(item)"
                 alt=""
               >
               <!-- 不是图片的文件兼容 -->
               <div
-                v-if="item.status === 'success' && item.type !== 'image' && !isIE9"
+                v-if="item.status === 'success' && item.fileType !== 'image' && !isIE9"
                 class="sp-upload-picture__item-info-file"
               >
                 <i class="sp-icon-file"></i>
                 <div class="sp-upload-picture__item-info-name">
-                  {{ item.name }}
+                  {{ item.fileName }}
                 </div>
               </div>
               <!-- 上传中 -->
@@ -230,7 +230,7 @@ export default {
     // 上传时http参数的文件参数名
     name: {
       type: String,
-      default: 'myFile'
+      default: 'file'
     },
     // 上传地址
     action: {
@@ -369,7 +369,7 @@ export default {
       if (item.type === 'image') {
         this.$sparta.imgPreview(this._getImgUrl(item))
       } else {
-        window.open(item.url)
+        window.open(item.fileUrl)
       }
     },
 
@@ -449,7 +449,7 @@ export default {
           const file = this._getFile(rawFile)
           this.onSuccess(res, rawFile)
           res = this.processResult(res)
-          file.url = res
+          file.fileUrl = res
           file.status = 'success'
           this._emitChange()
           delete this.request[uid]
@@ -525,11 +525,11 @@ export default {
       let file = {
         status: 'ready',
         uid: rawFile.uid,
-        name: rawFile.name,
+        fileName: rawFile.name,
         size: rawFile.size,
         percentage: 0,
         raw: rawFile,
-        type: rawFile.type.startsWith('image') ? 'image' : 'file'
+        fileType: rawFile.type.startsWith('image') ? 'image' : 'file'
       }
       // 预显示
       if (this.type === 'picture' && !this.isIE9) {
@@ -552,9 +552,9 @@ export default {
         const item = copy[i]
         if (item.status === 'success') {
           let data = {
-            name: item.name,
-            url: item.url,
-            type: item.type
+            fileName: item.fileName,
+            fileUrl: item.fileUrl,
+            fileType: item.fileType
           }
           rst.push(data)
         }
@@ -569,10 +569,10 @@ export default {
       for (let i = 0, len = copy.length; i < len; i++) {
         const item = copy[i]
         let data = {
-          name: item.name,
-          url: item.url,
+          fileName: item.fileName,
+          fileUrl: item.fileUrl,
           status: item.status,
-          type: item.type
+          fileType: item.fileType
         }
         if (this.type === 'picture' && !this.isIE9) {
           data.urlBase64 = item.urlBase64
@@ -593,7 +593,7 @@ export default {
       const file = {
         uid: Date.now() + this.uidIndex++,
         status: 'ready',
-        name: namePieces[namePieces.length - 1]
+        fileName: namePieces[namePieces.length - 1]
       }
       this.uploadFiles.push(file)
       const now = Date.now()
@@ -639,7 +639,7 @@ export default {
       try {
         const resData = iframe.contentDocument.body.textContent || iframe.contentWindow.document.body.textContent
         // 处理数据 。。。
-        this.uploadFiles[index].url = this.processResult(JSON.parse(resData))
+        this.uploadFiles[index].fileUrl = this.processResult(JSON.parse(resData))
         this.uploadFiles[index].status = 'success'
         this._emitChange()
       } catch (e) {
@@ -666,7 +666,7 @@ export default {
     },
 
     _getImgUrl(item) {
-      return (this.isIE9 || !item.urlBase64) ? item.url : item.urlBase64
+      return (this.isIE9 || !item.urlBase64) ? item.fileUrl : item.urlBase64
     },
 
     _parsePercentage(val) {
